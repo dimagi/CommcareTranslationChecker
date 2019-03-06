85e825aa50f19f2faebc76bfdeaad7c5e0657cf8
mkangia <mkangia@dimagi.com>
2019-03-06 13:50:28 +0530

    Upgrade openpyxl version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['openpyxl==2.0.5'],
+    install_requires=['openpyxl==2.6.0'],
 
 
     # To provide executable scripts, use entry points in preference to the
