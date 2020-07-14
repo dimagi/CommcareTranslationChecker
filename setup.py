32b0457fd94b8ee53938e169cbbf8bbaffbef601
Manish Kangia <mkangia@dimagi.com>
2020-07-14 17:01:02 +0530

    revert to old ways

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=requirements,
+    install_requires=['openpyxl>=2.6.1'],
 
 
     # To provide executable scripts, use entry points in preference to the
